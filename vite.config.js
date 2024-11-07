aa1330fcadabbffb066e45a0b1a615261f5f018a
Aaron Huynh <Aaronti2002@gmail.com>
2024-11-07 13:49:19 -0800

    project is unfinished, could not implement search or checkout

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ghPages } from 'vite-plugin-gh-pages'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/2107-Aaron-Midterm/',
+  base: '/',
+  build: {
+    outDir: 'build'
+  }
 })
